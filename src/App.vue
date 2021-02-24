8e31ddd7402922caea20d6e87cd4456839934f71
saksham-malhotra <malhotra.saksham@gmail.com>
2021-02-24 17:35:05 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld @click="showMessage"/>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+  methods:{
+    showMessage() {
+      alert('Yay');
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="showMessage">Hi</button>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  methods:{
+    showMessage() {
+      alert('Meh');
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
